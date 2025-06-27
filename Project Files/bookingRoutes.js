f2df2f5e1ac4787970fd588b01f5ab0c445ea14f
bandaru630 <Vamsikrishnabandaru970@gmail.com>
2025-06-27 14:20:41 +0530

    Delete Project Files directory

--- Project Files/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createBooking, getBookingsByUser } = require('../controllers/bookingController');
-
-router.post('/', createBooking);
-router.get('/user/:userId', getBookingsByUser);
-
-module.exports = router;
--- Project Files/propertyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createProperty, getAllProperties, getPropertyById } = require('../controllers/propertyController');
-
-router.post('/', createProperty);
-router.get('/', getAllProperties);
-router.get('/:id', getPropertyById);
-
-module.exports = router;
